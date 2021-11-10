91e1ce3bdc1592d080d37c5a1a9af96e7d8c4b75
LNJaideep2002 <87214924+LNJaideep2002@users.noreply.github.com>
2021-11-10 19:12:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.use(fileupload());
 mongoose.connect('mongodb://localhost/cse',{useNewUrlParser: true, useUnifiedTopology: true});
 var schema_login=mongoose.Schema({username: {type:String, unique: true },email: String,password: String});
 var model_login=mongoose.model('login',schema_login);
-var schema_teacher=mongoose.Schema({name: String,username: {type:String, unique: true },email: String,post: String,subject: Array});
+var schema_teacher=mongoose.Schema({name: String,username: {type:String, unique: true },email: String,post: String});
 var model_teacher=mongoose.model('teacher',schema_teacher);
 var schema_batch=mongoose.Schema({Semester: Number,exam: Array,file: Array});
 var model_batch=mongoose.model('batch',schema_batch);
+var schema_subject=mongoose.Schema({Subject: Array});
+var model_subject=mongoose.model('subjects',schema_subject);
 app.use(express.static('public'));
 app.set('view engine','ejs');
 var user={
@@ -25,8 +27,8 @@ var user={
     email:"",
     name:"",
     post:"",
+    generatedfile:"",
 }
-var generatedfile="";
 app.listen(6500,function()
 {
     console.log("server stated");
@@ -130,203 +132,264 @@ app.post("/password",function (req,res){
         })
     }
 });
-app.get("/generatefile",function(req,res){
-    var batch=[];
-    var semester=[]
-    var exam=["","","","","","","",""];
-    model_teacher.find({username:user.username},function(err,data){
-        for(i=0;i<data[0].subject.length;i++)
-        {
-            batch.push(data[0].subject[i].split("#")[0]);
-            semester.push(data[0].subject[i].split("#")[1]);
-        }
-        var filemis=[];
-        model_batch.find({},function(err,data1){
-            for(i=0;i<data1.length;i++)
-                for(j=0;j<batch.length;j++)
-                    for(k=0;k<data1[i].exam.length;k++)
-                        if(batch[j]==data1[i].exam[k].split("#")[0])
-                        exam[i]+=data1[i].exam[k]+"@";
-                        res.render("generatefile",{exam,subject:data[0].subject,username:user.username,status:0,filename:"sample#",stat:0,filemis1:filemis
-                    });
-        });
-    });
+app.get("/addsubject",function(req,res)
+{
+    res.render("aaddsubject",{...user,stats:0});
 });
-app.post("/generate",function(req,res) {
-    model_batch.find({Semester:Number(req.body.semester.split(" ")[1])},function(err,data0)
+app.post("/addsubject1",function(req,res){
+    if(req.body.addsub==undefined)
     {
-    var exa1=[];
-        for(i=0;i<data0[0].exam.length;i++)
-        if(data0[0].exam[i].split(req.body.batch+"#")[1]!=undefined)
-            exa1.push(data0[0].exam[i].split(req.body.batch+"#")[1]);
-            var file0=[];
-            for(i=0;i<data0[0].file.length;i++)
-            if(data0[0].file[i].split(user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_")[1].split(".")[0]!=undefined)
-            file0.push(data0[0].file[i].split(user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_")[1].split(".")[0]); 
-            var filemis=[];
-            var t=0;
-            for(i=0;i<exa1.length;i++)
-            { t=0;
-                for(j=0;j<file0.length;j++)
-                    if(exa1[i]==file0[j])
-                    {
-                        t=1;
-                    }
-                    if(t==0)
-                    filemis.push(exa1[i]+".xlsx file is missing");
+        var file=req.files.file;
+        filename="regulation"+req.body.Reg+".xlsx";
+        file.mv("./public/"+filename,function(err)
+        {
+            var wb=xl.readFile("./public/"+filename);
+            var ws=wb.Sheets["Sheet1"]; 
+            var data1= xl.utils.sheet_to_json(ws);
+            var batch=[];
+            req.body.Reg=Number(req.body.Reg);
+            console.log(data1);
+            for(i=0;i<4;i++)
+            {
+                batch.push(req.body.Reg+i+"-"+(req.body.Reg+i+4));
             }
-            console.log(filemis);
-            t=0;
-            for(i=0;i<data0[0].file.length;i++)
-                if(data0[0].file[i]==(user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"pos.xlsx"))
-                t=1;
-                if(t==0)
-                filemis.push(req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"pos.xlsx file is missing");
-                if(filemis.length==0)
-               generatedfile=excel.generatefile("./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"iae1.xlsx",
-               "./public/"+ user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"iae2.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"iae3.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"iae4.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"iae5.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"model.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"universityExam.xlsx",
-                "./public/"+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"pos.xlsx",
-                req.body.target);
-              var filename="sendfile";
-    var batch=[];
-    var semester=[]
-    var exam=["","","","","","","",""];
-    model_teacher.find({username:user.username},function(err,data){
-        for(i=0;i<data[0].subject.length;i++)
+            var subjects=[];
+            for(i=0;i<4;i++)
+            {
+                for(j=0;j<data1.length;j++)
+                {
+                    subjects.push(req.body.depart+"#"+batch[i]+"#"+"Semester "+data1[j].Semester+"#"+data1[j].Subject+"#"+data1[j].cos);
+                }
+            }
+            var newmodel=new model_subject;
+            newmodel.id=1;
+            newmodel.Subject=subjects;
+            model_subject.find({id:1},function(err,data)
+            {
+                console.log(data);
+                for(i=0;i<data[0].length;i++)
+                newmodel.Subject.push(data[0].Subject[i]);
+                // console.log(newmodel.Subject)
+                model_subject.remove({id:1},function(){
+                });
+                newmodel.save(function(err)
+                {
+                    console.log("updated");
+                })
+            });
+            res.render("aaddsubject",{...user,stats:1});
+        })
+    }else{
+        var subject=req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.sub+"#"+req.body.co;
+        var newmodel=new model_subject;
+        newmodel.id=1;
+        newmodel.Subject=[];
+        model_subject.find({id:1},function(err,data)
         {
-            batch.push(data[0].subject[i].split("#")[0]);
-            semester.push(data[0].subject[i].split("#")[1]);
+            // console.log(data);
+            newmodel.Subject=data[0].Subject;
+            newmodel.Subject.push(subject);
+            console.log(newmodel.Subject)
+            model_subject.remove({id:1},function(){
+            });
+            newmodel.save(function(err)
+            {
+                console.log("updated");
+            })
+        });
+        res.render("aaddsubject",{...user,stats:1});
+    }
+});
+app.get("/addteacher",function(req,res)
+{
+    res.render("aaddtecher",{st:2,username:user.username});
+});
+app.post("/aaddteacherdata",function(req,res)
+{
+    var newlogin=new model_login;
+    var newteach=new model_teacher;
+    newlogin.username=req.body.uname;
+    newlogin.email=req.body.email;
+    newlogin.password=req.body.pass;
+    newteach.username=req.body.uname;
+    newteach.email=req.body.email;
+    newteach.name=req.body.name;
+    newteach.post=req.body.post;
+    var t=0;
+    newlogin.save(function(err)
+    {
+        t=1;
+        newteach.save(function(err){
+            t=2
+            if(t==0)
+            {
+                res.render("aaddtecher",{st:1,username:user.username});
+            }
+            if(t==2)
+            {
+                res.render("aaddtecher",{st:0,username:user.username});
+            }
+        });
+    
+    });
+});
+app.get("/addexam",function(req,res)
+{
+    model_subject.find({id:1},function(err,data)
+    {
+    res.render("addexam",{status:0,username:user.username,subjects:data[0].Subject})
+    });
+});
+app.post("/exam",function(req,res)
+{
+
+    var exam=req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+"#"+req.body.exam;
+    var cos="[";
+    for(i in req.body)
+    {
+        if(i!="batch"&&i!="depart"&&i!="semester"&&i!="subject"&&i!="exam")
+        {
+            cos+=i;
+            cos+="-";
         }
-        model_batch.find({},function(err,data1){
+    }
+    cos+="]";
+    exam+=cos;
+    // console.log(exam);
+    model_batch.findOneAndUpdate({Semester:Number(req.body.semester.split(" ")[1])},{$push:{exam}},{upsert: true},function()
+    {
+        model_subject.find({id:1},function(err,data)
+    {
+    res.render("addexam",{status:1,username:user.username,subjects:data[0].Subject})
+    });
+    })
+})
+app.get("/uploadfile",function(req,res)
+{
+    model_subject.find({id:1},function(err,data)
+    {
+        model_batch.find({},function(err,data1)
+        {
+            var exam=[];
             for(i=0;i<data1.length;i++)
-                for(j=0;j<batch.length;j++)
-                    for(k=0;k<data1[i].exam.length;k++)
-                        if(batch[j]==data1[i].exam[k].split("#")[0]) 
-                        exam[i]+=data1[i].exam[k]+"@";
-                        if(filemis.length>0)
-                        res.render("generatefile",{exam,subject:data[0].subject,username:user.username,stat:0,filename,status:0,filemis1:filemis});//stat to bring file download icon // status to bring alert
-                        res.render("generatefile",{exam,subject:data[0].subject,username:user.username,stat:1,filename,status:1,filemis1:filemis});//stat to bring file download icon // status to bring alert
-                        
+            {
+                for(j=0;j<data1[i].exam.length;j++)
+                exam.push(data1[i].exam[j]);
+            }
+            res.render("uploadfile",{username:user.username,subjects:data[0].Subject,exam,status:0});
         });
     });
 });
-});
-app.get("/uploadfile",function(req,res) {
-    var batch=[];
-    var semester=[]
-    var exam=["","","","","","","",""];
-    model_teacher.find({username:user.username},function(err,data){
-        for(i=0;i<data[0].subject.length;i++)
-        {
-            batch.push(data[0].subject[i].split("#")[0]);
-            semester.push(data[0].subject[i].split("#")[1]);
-        }
-        model_batch.find({},function(err,data1){
-            for(i=0;i<data1.length;i++)
-                for(j=0;j<batch.length;j++)
-                    for(k=0;k<data1[i].exam.length;k++)
-                        if(batch[j]==data1[i].exam[k].split("#")[0])
-                        exam[i]+=data1[i].exam[k]+"@";
-                        res.render("uploadfile",{exam,subject:data[0].subject,username:user.username,status:0});
-        });
-    });
-});
-app.post("/uploading",function(req,res) {
-    var file=req.files.file;
-    console.log(file.name);
-    console.log(Number(req.body.semester.split(" ")[1]));
+app.post("/uploading",function(req,res)
+{
     if(req.body.pos==undefined)
     {
-        var filename=""+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+req.body.exam+".xlsx";   
-        console.log(filename);
+    var filename=req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+"#"+req.body.exam+".xlsx";
+    }else{ var filename="Pos"+"#"+req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+".xlsx";
     }
-    else
-    var filename=""+user.username+"_"+req.body.batch+"_"+req.body.semester+"_"+req.body.subject+"_"+"pos.xlsx";
+    var file=req.files.file;
     file.mv("./public/"+filename,function(err){
         model_batch.findOneAndUpdate({Semester:Number(req.body.semester.split(" ")[1])},{$push:{file:filename}},{upsert: true},function()
         {
-            var batch=[];
-            var semester=[]
-            var exam=["","","","","","","",""];
-            model_teacher.find({username:user.username},function(err,data){
-                for(i=0;i<data[0].subject.length;i++)
+            model_subject.find({id:1},function(err,data)
+            {
+                model_batch.find({},function(err,data1)
                 {
-                    batch.push(data[0].subject[i].split("#")[0]);
-                    semester.push(data[0].subject[i].split("#")[1]);
-                }
-                model_batch.find({},function(err,data1){
+                    var exam=[];
                     for(i=0;i<data1.length;i++)
-                        for(j=0;j<batch.length;j++)
-                            for(k=0;k<data1[i].exam.length;k++)
-                                if(batch[j]==data1[i].exam[k].split("#")[0])
-                                exam[i]+=data1[i].exam[k]+"@";
-                                res.render("uploadfile",{exam,subject:data[0].subject,username:user.username,status:1});
+                    {
+                        for(j=0;j<data1[i].exam.length;j++)
+                        exam.push(data1[i].exam[j].split("#")[0]+"#"+data1[i].exam[j].split("#")[1]+"#Semester "+data1[i].Semester+"#"+data1[i].exam[j].split("#")[2]+"#"+data1[i].exam[j].split("#")[3]);
+                    }
+                    res.render("uploadfile",{username:user.username,subjects:data[0].Subject,exam,status:1});
                 });
             });
         });
     });
 });
-app.get("/addexam",function(req,res){
-    res.render("addexam",{status:0,username:user.username});
-});
-app.post("/exam",function(req,res){
-    var exam=""+req.body.batch+"#"+req.body.exam;
-    model_batch.findOneAndUpdate({Semester:Number(req.body.semester.split(" ")[1])},{$push:{exam}},{upsert: true},function()
+app.get("/generatefile",function(req,res)
+{
+    var filemiss=[]
+    model_subject.find({id:1},function(err,data)
     {
-        res.render("addexam",{status:1,username:user.username});
+        res.render("generatefile",{username:user.username,subjects:data[0].Subject,filemiss});
+    });
+});
+app.post("/generate",function(req,res)
+{
+    console.log(req.body);
+    model_batch.find({},function(err,data){
+        var exams1=[];
+        var files=[];
+        for(i=0;i<data.length;i++)
+        {
+            if(data[i].Semester==(req.body.semester.split(" ")[1]))
+            {
+                exams1=data[i].exam;
+                files=data[i].file;
+            }
+        }
+        var exams=[];
+        for(i=0;i<exams1.length;i++)
+        {
+            var res1=exams1[i].search(req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+"#")
+            if(res1!=-1)
+            {
+                exams.push(exams1[i]);
+            }
+        }
+        // console.log(exams);
+        var filemiss=[];
+        var fileavail=[];
+        var t=0;
+        for(i=0;i<exams.length;i++)
+        {
+            t=0;
+            for(j=0;j<files.length;j++)
+            {
+                if(files[j]==(exams[i]+".xlsx"))
+                {
+                    fileavail.push(files[j]);
+                    t=1;
+                    break;
+                }
+            }
+            if(t!=1)
+            {
+                filemiss.push(exams[i]+".xlsx")
+            }
+        }
+        t=0;
+        for(j=0;j<files.length;j++)
+        {
+            if(files[j]==("Pos"+"#"+req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+".xlsx"))
+            t=1;
+        }
+        if(t==0)
+        filemiss.push("Pos"+"#"+req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+".xlsx")
+        if(filemiss.length>0)
+        {
+            // console.log(filemiss)
+            model_subject.find({id:1},function(err,data)
+                {
+                    excel.generatefile();
+                    res.render("generatefile",{username:user.username,subjects:data[0].Subject,filemiss});
+                });
+        }
+        else{
+            model_subject.find({},function(err,data1)
+            {
+                var nocos=0;
+                for(i=0;i<data1[0].Subject;i++)
+                {
+                    var res1=data1[0].Subject[i].search(req.body.depart+"#"+req.body.batch+"#"+req.body.semester+"#"+req.body.subject+"#");
+                    if(res1!=-1)
+                    {
+                        nocos=Number(data1[0].Subject[i].split("#")[4]);
+                    }
+                }
+                excel.generatefile(nocos,exams,fileavail,req.body);
+            })
+        }
     })
-})
-app.get("/addteacher",function(req,res)
-{
-    res.render("aaddtecher",{st:2,username:user.username});
-});
-app.post("/aaddteacherdata",async function(req,res)
-{
-    var new1=req.body.new;
-    if(new1==undefined)
-    req.body={...req.body,new:"off"};
-    var sem=req.body.semester.split(" ")[0]+req.body.semester.split(" ")[1];
-    var sub=""+req.body.batch+"#"+sem+"#"+req.body.subject;
-    if(req.body.new=="off")
-    {
-        model_teacher.findOneAndUpdate({username:req.body.uname},{ $push: {subject:[sub]}},{upsert: true},function()
-        {
-            res.render("aaddtecher",{st:0,username:user.username});
-        });
-    }
-    else{
-        var teacher=new model_teacher();
-        teacher.username=req.body.uname;
-        teacher.email=req.body.email;
-        teacher.name=req.body.name;
-        teacher.post=req.body.post;
-        teacher.subject=[sub];
-        var login=new model_login();
-        login.username=req.body.uname;
-        login.password=req.body.pass;
-        login.email=req.body.email;
-        try{
-            login.save(function()
-            {
-            });
-            teacher.save(function()
-            {
-                
-            });
-            res.render("aaddtecher",{st:0,username:user.username});
-        }
-        catch(err)
-        {
-            res.render("aaddtecher",{st:1,username:user.username});
-        }
-    }
-});
-app.get("/sendfile",function(req,res)
-{
-    res.sendFile(__dirname+generatedfile);
 });
